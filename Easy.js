95569d0a4153401251712c4b61a16f1627c4bb9d
Kj McCray <kensvne@Kjs-MacBook-Pro.local>
2020-11-09 16:12:58 -0500

    Update

--- Easy.js
+++ Easy.js
@@ -1,1 +1,5 @@
-let students = [["Jermaine Cole", "35"], ["Kendrick Lamar", "33"], ["Travis Scott", "29"]];
+
+var students = [["Jermaine Cole"], ["Kendrick Lamar"], ["Travis Scott"]]
+var studentsAge = [[35], [33], [29]]
+
+console.log(" Hello my name is " + students[1] + " and I'm " + studentsAge[1] + " years old. ")
---
+++ Medium.js
@@ -0,0 +1,5 @@
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+function processUSerInput(Number) {
+    console.log(prompt("Enter a Number"))
+} 
