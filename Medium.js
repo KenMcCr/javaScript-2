89e97d2c060916d8a19d3e6ca4a1a3d22facfb48
Kj McCray <kensvne@Kjs-MacBook-Pro.local>
2020-11-09 22:51:44 -0500

    Update

--- Medium.js
+++ Medium.js
@@ -1,5 +1,21 @@
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// let months = ["null", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-function processUSerInput(Number) {
-    console.log(prompt("Enter a Number"))
-} 
+
+var months = [];
+months[1] = 'January';
+months[2] = 'February';
+months[3] = 'March';
+months[4] = 'April';
+months[5] = 'May';
+months[6] = 'June';
+months[7] = 'July';
+months[8] = 'August';
+months[9] = 'September';
+months[10] = 'October';
+months[11] = 'November';
+months[12] = 'December';
+
+console.log(months[1]);
+console.log(months[5]);
+console.log(prompt(" Please enter a number. "));
+
